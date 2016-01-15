7aeee2b854071c706989498914792f3162239cb5
Devin Shoemaker <devinshoe@gmail.com>
2016-01-15 06:35:41 -0600

    Create Player class

---
+++ app/src/main/java/com/devinshoemaker/pingpongscore/Player.java
@@ -0,0 +1,11 @@
+package com.devinshoemaker.pingpongscore;
+
+/**
+ * Created by Devin Shoemaker on 1/15/16.
+ *
+ * Used to managed each player and their score
+ */
+public class Player {
+    private String name;
+    private int score;
+}
