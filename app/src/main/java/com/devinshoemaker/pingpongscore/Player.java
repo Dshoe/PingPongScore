88bb49315c3aa41f2057eae1df471e6c2c77294a
Devin Shoemaker <devinshoe@gmail.com>
2016-01-15 06:36:35 -0600

    Add getter and setter for Player

--- app/src/main/java/com/devinshoemaker/pingpongscore/Player.java
+++ app/src/main/java/com/devinshoemaker/pingpongscore/Player.java
@@ -8,4 +8,20 @@ package com.devinshoemaker.pingpongscore;
 public class Player {
     private String name;
     private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
