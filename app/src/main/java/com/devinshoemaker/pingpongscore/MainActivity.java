80f2e45d9455c8b392394036c96f17a436fa5806
Devin Shoemaker <devinshoe@gmail.com>
2016-01-18 21:37:38 -0600

    Fix type

--- app/src/main/java/com/devinshoemaker/pingpongscore/MainActivity.java
+++ app/src/main/java/com/devinshoemaker/pingpongscore/MainActivity.java
@@ -2,7 +2,6 @@ package com.devinshoemaker.pingpongscore;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private enum states {
         END_GAME,
         IN_PROGRESS,
-        IN_PROGRESS_SWTICHED
+        IN_PROGRESS_SWITCHED
     }
 
     private String currentState = states.END_GAME.toString();
@@ -125,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
             playerRight = playerOne;
             tvLeftPlayer.setText(playerLeft.getScore());
             tvRightPlayer.setText(playerRight.getScore());
-        } else if (isAllowed(states.IN_PROGRESS_SWTICHED)) {
+        } else if (isAllowed(states.IN_PROGRESS_SWITCHED)) {
             playerLeft.setScore(0);
             playerRight.setScore(0);
             playerOne = playerRight;
